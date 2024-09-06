6bd4aa15e3984b67612b2f4614f420799a9677a6
Bradley-Hill <bradley.loizeau@gmail.com>
2024-09-06 16:20:10 +0200

    removed unnecessary import

--- memory-game/app/page.tsx
+++ memory-game/app/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React, { useEffect, useState } from "react";
+import React, { useEffect } from "react";
 import { fetchCardData } from "./utils/api";
 import GameBoard from "./components/GameBoard";
 import ScoreBoard from "./components/ScoreBoard";
