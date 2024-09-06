ee890463bb7de6366bba0c059a74df5ece235a0b
Bradley-Hill <bradley.loizeau@gmail.com>
2024-09-06 16:17:29 +0200

    added the memorycard to display card name and art, and then used the fetchCardData in page.tsx

--- memory-game/app/components/MemoryCard.tsx
+++ memory-game/app/components/MemoryCard.tsx
@@ -1,5 +1,26 @@
 import React from "react";
+import Image from "next/image";
 
-export default function MemoryCard() {
-  return <div></div>;
+interface MemoryCardProps {
+  name: string;
+  imageUrl: string;
 }
+
+const MemoryCard: React.FC<MemoryCardProps> = ({ name, imageUrl }) => {
+  const width = 200;
+  const height = 200;
+  return (
+    <div className="MemoryCard">
+      <Image
+        src={imageUrl}
+        alt={name}
+        layout="responsive"
+        width={width}
+        height={height}
+      />
+      <h2>{name}</h2>
+    </div>
+  );
+};
+
+export default MemoryCard;
--- memory-game/app/page.tsx
+++ memory-game/app/page.tsx
@@ -1,15 +1,49 @@
-import React from "react";
+"use client";
+
+import React, { useEffect, useState } from "react";
+import { fetchCardData } from "./utils/api";
 import GameBoard from "./components/GameBoard";
 import ScoreBoard from "./components/ScoreBoard";
 import DifficultySelector from "./components/DifficultySelector";
+import MemoryCard from "./components/MemoryCard";
 
-export default function Home() {
+interface CardData {
+  name: string;
+  image_uris: {
+    art_crop: string;
+  };
+}
+
+interface HomeProps {
+  initialCardData: CardData | null;
+}
+
+export default function Home({ initialCardData }: HomeProps) {
+  const [cardData, setCardData] = React.useState<CardData | null>(
+    initialCardData
+  );
+
+  useEffect(() => {
+    if (!initialCardData) {
+      fetchCardData("Black Lotus")
+        .then((data) => setCardData(data))
+        .catch((error) => console.error("Error fetching card data: ", error));
+    }
+  }, [initialCardData]);
+
+  if (!cardData) {
+    return <div>Error loading card data</div>;
+  }
+
+  const artCropUrl = cardData.image_uris.art_crop;
+
   return (
     <div className="container mx-auto p-4">
       <h1 className="text-3xl font-bold text-center mb-4">Magic Memory Game</h1>
       <DifficultySelector />
       <ScoreBoard />
       <GameBoard />
+      <MemoryCard name={cardData.name} imageUrl={artCropUrl} />
     </div>
   );
 }
