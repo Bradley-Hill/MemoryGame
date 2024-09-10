e76e93092c6f85216a5a7e8aefb51014ec72ce3d
Bradley-Hill <bradley.loizeau@gmail.com>
2024-09-10 16:56:12 +0200

    fixed the api.ts to be more verbose with error logging. fixed cardArray list to have correct names for api calls.

--- memory-game/app/utils/api.ts
+++ memory-game/app/utils/api.ts
@@ -16,8 +16,25 @@ export const fetchCardData = async (cardName: string) => {
       },
     });
     return response.data;
-  } catch (error) {
-    console.error("Error fetching card data: ", error);
-    throw error;
+  } catch (error: any) {
+    if (error.response) {
+      // The request was made and the server responded with a status code
+      // that falls out of the range of 2xx
+      console.error("Error fetching card data:", error.response.data);
+      throw new Error(
+        `Error fetching card data: ${error.response.data.details}`
+      );
+    } else if (error.request) {
+      // The request was made but no response was received
+      console.error(
+        "Error fetching card data: No response received",
+        error.request
+      );
+      throw new Error("Error fetching card data: No response received");
+    } else {
+      // Something happened in setting up the request that triggered an Error
+      console.error("Error fetching card data:", error.message);
+      throw new Error(`Error fetching card data: ${error.message}`);
+    }
   }
 };
